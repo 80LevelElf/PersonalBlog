7b9c648a2f7b8ffbfc2eff2e10f3260bcc812371
80LevelElf <salahutd@gmail.com>
2016-08-07 00:09:54 +0300

    - A lot of changed in AboutMe page
    Signed-off-by: 80LevelElf <salahutd@gmail.com>

--- PersonalBlog/App_Start/BundleConfig.cs
+++ PersonalBlog/App_Start/BundleConfig.cs
@@ -1,11 +1,9 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace PersonalBlog
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -26,18 +24,9 @@ namespace PersonalBlog
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css/blog").Include("~/Content/Blog.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(                        
+                        "~/Content/themes/base/jquery-ui.css",
+                        "~/Content/themes/base/jquery-ui.structure.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
---
+++ PersonalBlog/Controllers/AboutMeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using NodaTime;
+
+namespace PersonalBlog.Controllers
+{
+    public class AboutMeController : Controller
+    {
+        //
+        // GET: /AboutMe/
+
+        public ActionResult Index()
+        {
+            ViewBag.AgeInYears = (int)Period.Between(
+                new LocalDateTime(1995, 4, 29, 0, 0),
+                LocalDateTime.FromDateTime(DateTime.Now)).Years;
+
+            return View();
+        }
+
+    }
+}
