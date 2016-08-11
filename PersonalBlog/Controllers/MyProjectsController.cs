8c3f20e1e40a10025a627c83a4c56556e448621f
80LevelElf <salahutd@gmail.com>
2016-08-12 00:30:26 +0300

    Starting work of "my projects" page
    Signed-off-by: 80LevelElf <salahutd@gmail.com>

---
+++ PersonalBlog/Controllers/MyProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalBlog.Controllers
+{
+    public class MyProjectsController : Controller
+    {
+        //
+        // GET: /MyProject/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
