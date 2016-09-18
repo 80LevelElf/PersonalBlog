7985d402f23e2d8f2e995ee9fc9d4608f3e3a49d
80LevelElf <salahutd@gmail.com>
2016-09-18 21:55:38 +0300

    Turn off minimizing of bundles, because of huge memory usage
    Signed-off-by: 80LevelElf <salahutd@gmail.com>

--- PersonalBlog/App_Start/BundleConfig.cs
+++ PersonalBlog/App_Start/BundleConfig.cs
@@ -7,53 +7,53 @@ namespace PersonalBlog
         public static void RegisterBundles(BundleCollection bundles)
         {
             //Scripts
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+			bundles.Add(new Bundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+			bundles.Add(new Bundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/markdown-editor")
+			bundles.Add(new Bundle("~/bundles/markdown-editor")
                 .Include("~/Scripts/markdown/simplemde.js")
                 .Include("~/Scripts/markdown/md-editor.js"));
 
             //Turn off minification of this bundle, because it's crush all :(
-            var markDownBundle = new ScriptBundle("~/bundles/markdown").Include(
+			var markDownBundle = new Bundle("~/bundles/markdown").Include(
                 "~/Scripts/markdown/markdown.js");
             markDownBundle.Transforms.Clear();
             bundles.Add(markDownBundle);
 
-            bundles.Add(new ScriptBundle("~/bundles/utils").Include("~/Scripts/utils.js"));
-			bundles.Add(new ScriptBundle("~/bundles/highlight").Include("~/Scripts/highlight.js"));
-			bundles.Add(new ScriptBundle("~/bundles/postManager").Include("~/Scripts/postManager.js"));
+			bundles.Add(new Bundle("~/bundles/utils").Include("~/Scripts/utils.js"));
+			bundles.Add(new Bundle("~/bundles/highlight").Include("~/Scripts/highlight.js"));
+			bundles.Add(new Bundle("~/bundles/postManager").Include("~/Scripts/postManager.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/addOrUpdatePost").Include("~/Scripts/pages/addOrUpdatePost.js"));
-            bundles.Add(new ScriptBundle("~/bundles/postPreview").Include("~/Scripts/pages/postPreview.js"));
-			bundles.Add(new ScriptBundle("~/bundles/post").Include("~/Scripts/pages/post.js"));
+			bundles.Add(new Bundle("~/bundles/addOrUpdatePost").Include("~/Scripts/pages/addOrUpdatePost.js"));
+			bundles.Add(new Bundle("~/bundles/postPreview").Include("~/Scripts/pages/postPreview.js"));
+			bundles.Add(new Bundle("~/bundles/post").Include("~/Scripts/pages/post.js"));
 
             //CSS
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-			bundles.Add(new StyleBundle("~/Content/css/blog").Include("~/Content/blog.css"));
-            bundles.Add(new StyleBundle("~/Content/css/aboutMe").Include("~/Content/pages/aboutMe.css"));
-			bundles.Add(new StyleBundle("~/Content/css/myProjects").Include("~/Content/pages/myProjects.css"));
-            bundles.Add(new StyleBundle("~/Content/css/post-previews")
+			bundles.Add(new Bundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new Bundle("~/Content/css/blog").Include("~/Content/blog.css"));
+			bundles.Add(new Bundle("~/Content/css/aboutMe").Include("~/Content/pages/aboutMe.css"));
+			bundles.Add(new Bundle("~/Content/css/myProjects").Include("~/Content/pages/myProjects.css"));
+			bundles.Add(new Bundle("~/Content/css/post-previews")
 				.Include("~/Content/pages/post-previews.css")
 				.Include("~/Content/postMarkup.css"));
-            bundles.Add(new StyleBundle("~/Content/css/post")
+			bundles.Add(new Bundle("~/Content/css/post")
 				.Include("~/Content/pages/post.css")
 				.Include("~/Content/postMarkup.css"));
-            bundles.Add(new StyleBundle("~/Content/css/login").Include("~/Content/pages/login.css"));
-            bundles.Add(new StyleBundle("~/Content/css/adminLayout").Include("~/Content/pages/adminLayout.css"));
+			bundles.Add(new Bundle("~/Content/css/login").Include("~/Content/pages/login.css"));
+			bundles.Add(new Bundle("~/Content/css/adminLayout").Include("~/Content/pages/adminLayout.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css/markdown-editor").Include("~/Content/markdown/simplemde.css"));
+			bundles.Add(new Bundle("~/Content/css/markdown-editor").Include("~/Content/markdown/simplemde.css"));
 
-			bundles.Add(new StyleBundle("~/Content/css/highlightStyle").Include("~/Content/highlightStyles/vs.css"));
+			bundles.Add(new Bundle("~/Content/css/highlightStyle").Include("~/Content/highlightStyles/vs.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(      
+			bundles.Add(new Bundle("~/Content/themes/base/css").Include(      
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.button.css",
                         "~/Content/themes/base/jquery.ui.tabs.css",
